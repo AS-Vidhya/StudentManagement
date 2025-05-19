3c1337af719374560d79a170d01e0a145cb86f6f
Srividhya <assrividhya113@gmail.com>
2025-05-19 11:12:42 +0530

    Add model files

---
+++ StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement.Models
+{
+	public class StudentContext :  DbContext
+	{
+		public StudentContext() : base("StudentContext"){}
+		public DbSet<Student> Students { get; set; }
+	}
+}
